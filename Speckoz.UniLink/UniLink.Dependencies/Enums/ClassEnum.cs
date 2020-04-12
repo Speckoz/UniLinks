bc4ab6a4bedd14a176954e8e03b66ab83ef24b5b
Marco Antonio P <marcopandolfo577@gmail.com>
2020-04-12 07:55:37 -0300

    ➕ Criando Model do link

---
+++ Speckoz.UniLink/UniLink.Dependencies/Enums/ClassEnum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniLink.Dependencies.Enums
+{
+    public enum ClassEnum
+    {
+        
+    }
+}
---
+++ Speckoz.UniLink/UniLink.Dependencies/Models/Link.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniLink.Dependencies.Models
+{
+    public class Link
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Teacher { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
