bb3dc840dbb58efa7ccd8cd60ce70cd611d73c7c
Marco Antonio P <marcopandolfo577@gmail.com>
2020-04-16 06:25:11 -0300

    ➕ Criando Student Repository

---
+++ Speckoz.UniLink/UniLink.API/Repository/Interfaces/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniLink.Dependencies.Models;
+
+namespace UniLink.API.Repository.Interfaces
+{
+    public interface IStudentRepository
+    {
+        Task<StudentModel> FindTaskAsync(string email);
+        Task<StudentModel> AddTaskAsync(StudentModel student);
+        Task DeleteTaskAsync(StudentModel student);
+    }
+}
