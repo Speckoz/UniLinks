f1de1535108513b313a8e05471d57a4c020faabb
Marco Antonio P <marcopandolfo577@gmail.com>
2020-04-15 22:57:23 -0300

    🍴 Fixando Seeders

--- Speckoz.UniLink/UniLink.API/Data/DataSeeder.cs
+++ Speckoz.UniLink/UniLink.API/Data/DataSeeder.cs
@@ -17,142 +17,151 @@ namespace UniLink.API.Data
 
 		public void Init()
 		{
+			UserLoginModel u1 = null, u2 = null, u3 = null, u4 = null;
+			CourseModel c1 = null, c2 = null;
+			DisciplineModel d1 = null, d2 = null;
+			LessonModel l1 = null, l2 = null;
+			StudentModel s1 = null, s2 = null;
+
 			SeedUsers();
 			SeedCourse();
 			SeedDisciplines();
 			SeedLeassons();
 			SeedStudents();
-		}
 
-		private void SeedUsers()
-		{
-			if (!_context.Users.Any())
+			_context.SaveChanges();
+
+			void SeedUsers()
 			{
-				_context.Users.Add(new UserLoginModel
+				if (!_context.Users.Any())
 				{
-					UserId = Guid.Parse("DA418561-E1B0-41C9-955D-2B41F35F0C4F"),
-					Name = "Coord Eletrica",
-					Email = "coord.ee@unilink.com",
-					Password = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3",
-					UserType = UserTypeEnum.Coordinator
-				});
-				_context.Users.Add(new UserLoginModel
-				{
-					UserId = Guid.Parse("BBB85201-C532-43F8-9F53-FC1696F49088"),
-					Name = "Coord Sistemas",
-					Email = "coord.si@unilink.com",
-					Password = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3",
-					UserType = UserTypeEnum.Coordinator
-				});
+					u1 = new UserLoginModel
+					{
+						UserId = Guid.Parse("DA418561-E1B0-41C9-955D-2B41F35F0C4F"),
+						Name = "Coord Eletrica",
+						Email = "coord.ee@unilink.com",
+						Password = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3",
+						UserType = UserTypeEnum.Coordinator
+					};
 
-				_context.Users.Add(new UserLoginModel
-				{
-					UserId = Guid.Parse("74E8B9D9-310D-431B-B118-D386B85F8B8A"),
-					Name = "Ruan Carlos",
-					Email = "logikoz@unilink.com",
-					UserType = UserTypeEnum.Student
-				});
+					u2 = new UserLoginModel
+					{
+						UserId = Guid.Parse("BBB85201-C532-43F8-9F53-FC1696F49088"),
+						Name = "Coord Sistemas",
+						Email = "coord.si@unilink.com",
+						Password = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3",
+						UserType = UserTypeEnum.Coordinator
+					};
 
-				_context.Users.Add(new UserLoginModel
-				{
-					UserId = Guid.Parse("4E4F780A-0313-45F6-8897-3BFF7D930778"),
-					Name = "Marco Pandolfo",
-					Email = "specko@unilink.com",
-					UserType = UserTypeEnum.Student
-				});
+					u3 = new UserLoginModel
+					{
+						UserId = Guid.Parse("74E8B9D9-310D-431B-B118-D386B85F8B8A"),
+						Name = "Ruan Carlos",
+						Email = "logikoz@unilink.com",
+						UserType = UserTypeEnum.Student
+					};
 
-				_context.SaveChanges();
+					u4 = new UserLoginModel
+					{
+						UserId = Guid.Parse("4E4F780A-0313-45F6-8897-3BFF7D930778"),
+						Name = "Marco Pandolfo",
+						Email = "specko@unilink.com",
+						UserType = UserTypeEnum.Student
+					};
+
+					_context.Users.AddRange(u1, u2, u3, u4);
+				}
 			}
-		}
 
-		private void SeedCourse()
-		{
-			if (!_context.Courses.Any())
+			void SeedCourse()
 			{
-				_context.Courses.Add(new CourseModel
+				if (!_context.Courses.Any())
 				{
-					CourseId = Guid.Parse("8E6BDCE0-6B3C-4847-8EB7-0B5C4BC6549E"),
-					Name = "Engenharia Eletrica",
-					Periods = 10,
-					CoordinatorId = Guid.Parse("DA418561-E1B0-41C9-955D-2B41F35F0C4F")
-				});
-				_context.Courses.Add(new CourseModel
-				{
-					CourseId = Guid.Parse("498880FD-A708-4BD4-AAC7-4AD859DC1FD8"),
-					Name = "Sistemas de Informaçao",
-					Periods = 8,
-					CoordinatorId = Guid.Parse("BBB85201-C532-43F8-9F53-FC1696F49088")
-				});
+					c1 = new CourseModel
+					{
+						CourseId = Guid.Parse("8E6BDCE0-6B3C-4847-8EB7-0B5C4BC6549E"),
+						Name = "Engenharia Eletrica",
+						Periods = 10,
+						CoordinatorId = Guid.Parse("DA418561-E1B0-41C9-955D-2B41F35F0C4F")
+					};
+					c2 = new CourseModel
+					{
+						CourseId = Guid.Parse("498880FD-A708-4BD4-AAC7-4AD859DC1FD8"),
+						Name = "Sistemas de Informaçao",
+						Periods = 8,
+						CoordinatorId = Guid.Parse("BBB85201-C532-43F8-9F53-FC1696F49088")
+					};
 
-				_context.SaveChanges();
+					_context.Courses.AddRange(c1, c2);
+				}
 			}
-		}
 
-		private void SeedDisciplines()
-		{
-			if (!_context.Disciplines.Any())
+			void SeedDisciplines()
 			{
-				_context.Disciplines.Add(new DisciplineModel
+				if (!_context.Disciplines.Any())
 				{
-					DisciplineId = Guid.Parse("D02F5571-F056-4BFF-A5E0-A927306AE77D"),
-					Name = "Principios de Eletronica",
-					Teacher = "Son Goku",
-					Period = 5,
-					Course = _context.Courses.SingleOrDefault(x => x.CourseId == Guid.Parse("8E6BDCE0-6B3C-4847-8EB7-0B5C4BC6549E"))
-				});
-				//_context.Disciplines.Add(new DisciplineModel
-				//{
-				//	DisciplineId = Guid.Parse("10E2BABB-EB2A-4473-B9D9-499D9F595C43"),
-				//	Name = "Fundamentos de programaçao",
-				//	Teacher = "Naruto Uzumaki",
-				//	Period = 2,
-				//	Course = _context.Courses.SingleOrDefault(x => x.CourseId == Guid.Parse("BBB85201-C532-43F8-9F53-FC1696F49088"))
-				//});
+					d1 = new DisciplineModel
+					{
+						DisciplineId = Guid.Parse("D02F5571-F056-4BFF-A5E0-A927306AE77D"),
+						Name = "Principios de Eletronica",
+						Teacher = "Son Goku",
+						Period = 5,
+						Course = c1
+					};
+					d2 = new DisciplineModel
+					{
+						DisciplineId = Guid.Parse("10E2BABB-EB2A-4473-B9D9-499D9F595C43"),
+						Name = "Fundamentos de programaçao",
+						Teacher = "Naruto Uzumaki",
+						Period = 2,
+						Course = c2
+					};
 
-				_context.SaveChanges();
+					_context.Disciplines.AddRange(d1, d2);
+				}
 			}
-		}
 
-		private void SeedLeassons()
-		{
-			if (!_context.Lessons.Any())
+			void SeedLeassons()
 			{
-				_context.Lessons.Add(new LessonModel
+				if (!_context.Lessons.Any())
 				{
-					Date = DateTime.Now,
-					LessonSubject = "Lição",
-					URI = "https://logikoz.net",
-					Discipline = _context.Disciplines.SingleOrDefault(x => x.DisciplineId == Guid.Parse("D02F5571-F056-4BFF-A5E0-A927306AE77D"))
-				});
+					l1 = new LessonModel
+					{
+						Date = DateTime.Now,
+						LessonSubject = "Lição",
+						URI = "https://logikoz.net",
+						Discipline = d1
+					};
 
-				_context.Lessons.Add(new LessonModel
-				{
-					Date = DateTime.Now,
-					LessonSubject = "Prova",
-					URI = "https://bit.ly/2RF0oFQ",
-					Discipline = _context.Disciplines.SingleOrDefault(x => x.DisciplineId == Guid.Parse("10E2BABB-EB2A-4473-B9D9-499D9F595C43"))
-				});
+					l2 = new LessonModel
+					{
+						Date = DateTime.Now,
+						LessonSubject = "Prova",
+						URI = "https://bit.ly/2RF0oFQ",
+						Discipline = d2
+					};
 
-				_context.SaveChanges();
+					_context.Lessons.AddRange(l1, l2);
+				}
 			}
-		}
 
-		private void SeedStudents()
-		{
-			if (!_context.Students.Any())
+			void SeedStudents()
 			{
-				_context.Students.Add(new StudentModel
+				if (!_context.Students.Any())
 				{
-					UserId = Guid.Parse("74E8B9D9-310D-431B-B118-D386B85F8B8A"),
-					Course = _context.Courses.SingleOrDefault(x => x.CourseId == Guid.Parse("8E6BDCE0-6B3C-4847-8EB7-0B5C4BC6549E"))
-				});
-				_context.Students.Add(new StudentModel
-				{
-					UserId = Guid.Parse("4E4F780A-0313-45F6-8897-3BFF7D930778"),
-					Course = _context.Courses.SingleOrDefault(x => x.CourseId == Guid.Parse("BBB85201-C532-43F8-9F53-FC1696F49088"))
-				});
+					s1 = new StudentModel
+					{
+						UserId = Guid.Parse("74E8B9D9-310D-431B-B118-D386B85F8B8A"),
+						Course = c1
+					};
+					s2 = new StudentModel
+					{
+						UserId = Guid.Parse("4E4F780A-0313-45F6-8897-3BFF7D930778"),
+						Course = c2
+					};
 
-				_context.SaveChanges();
+					_context.Students.AddRange(s1, s2);
+				}
 			}
 		}
 	}
