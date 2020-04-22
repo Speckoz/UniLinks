ad76769749757fc1fd77c4f6af028fc7a6d76281
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-04-21 23:32:52 -0500

    ➕ Adicionando codigo para pegar frame do video [SIte] [Bug]

--- Speckoz.UniLink/UniLink.Client.Site/wwwroot/js/site.js
+++ Speckoz.UniLink/UniLink.Client.Site/wwwroot/js/site.js
@@ -32,3 +32,28 @@ function DisposeModal(modalId)
         $('#' + modalId).modal('dispose');
     });
 }
+
+function capture(video, scaleFactor)
+{
+    const w = video.videoWidth * scaleFactor;
+    const h = video.videoHeight * scaleFactor;
+
+    const canvas = document.getElementById('canvasid');
+    canvas.width = w;
+    canvas.height = h;
+
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, w, h);
+
+    const img = document.getElementById('canvasimg');
+    img.setAttribute('crossOrigin', 'anonymous');
+    //img.src = canvas.toDataURL('png', 0.8);
+}
+
+function shoot(time)
+{
+    const video = document.getElementById('video');
+    video.currentTime = time;
+
+    capture(video, 0.25);
+}
