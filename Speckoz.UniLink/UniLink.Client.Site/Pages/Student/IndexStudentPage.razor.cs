dfff9977712d767a518282ef867ff26f3950a6de
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-04-24 23:07:26 -0500

    ➕ Adicionando filtro por disciplina e periodo nas aulas do aluno [Site]

--- Speckoz.UniLink/UniLink.Client.Site/Pages/Student/IndexStudentPage.razor.cs
+++ Speckoz.UniLink/UniLink.Client.Site/Pages/Student/IndexStudentPage.razor.cs
@@ -3,6 +3,7 @@
 using Microsoft.AspNetCore.Components;
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using UniLink.Client.Site.Services.Student;
@@ -17,6 +18,18 @@ namespace UniLink.Client.Site.Pages.Student
 	{
 		private LessonDisciplineVO selected;
 		private IList<LessonDisciplineVO> lessons;
+		private IList<LessonDisciplineVO> lessonOrigin;
+		private byte filterSelected;
+
+		private byte FilterSelected
+		{
+			get => filterSelected;
+			set
+			{
+				filterSelected = value;
+				lessons = lessonOrigin;
+			}
+		}
 
 		[Inject]
 		private ISessionStorageService SessionStorage { get; set; }
@@ -24,9 +37,34 @@ namespace UniLink.Client.Site.Pages.Student
 		protected async override Task OnInitializedAsync()
 		{
 			string token = await SessionStorage.GetItemAsync<string>("token");
-			lessons = await new LessonService().GetAllLessonsTaskAync(token, "d02f5571-f056-4bff-a5e0-a927306ae77d;10e2babb-eb2a-4473-b9d9-499d9f595c43");
+			lessons = lessonOrigin = await new LessonService().GetAllLessonsTaskAync(token, "d02f5571-f056-4bff-a5e0-a927306ae77d;10e2babb-eb2a-4473-b9d9-499d9f595c43");
 		}
 
 		private void SelectLesson(LessonDisciplineVO lesson) => selected = lesson;
+
+		private void FilterSearch(ChangeEventArgs e)
+		{
+			string value = e.Value.ToString();
+			if (!string.IsNullOrEmpty(value))
+			{
+				switch (FilterSelected)
+				{
+					case 1:
+						lessons = lessonOrigin.Where(x => x.Discipline.Name.ToLower().Contains(value.ToLower())).ToList();
+						return;
+
+					case 2:
+						if (byte.TryParse(value, out byte result))
+							lessons = lessonOrigin.Where(x => x.Discipline.Period.Equals(result)).ToList();
+						return;
+
+					case 3:
+						//caso seja por data
+						return;
+				}
+			}
+
+			lessons = lessonOrigin;
+		}
 	}
 }
