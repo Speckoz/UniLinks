65e6bf58ef8a3743d84b69f26edbc1fe462857d1
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-04-27 14:37:28 -0500

    ➕ Adicionando nome do usuario logado no canto superior direito da pagina inicial [Site]

--- Speckoz.UniLink/UniLink.Client.Site/Pages/IndexPage.razor.cs
+++ Speckoz.UniLink/UniLink.Client.Site/Pages/IndexPage.razor.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.SessionStorage;
+
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace UniLink.Client.Site.Pages
 {
 	public partial class IndexPage
 	{
+		private string name;
+
 		[Inject]
 		private AuthenticationStateProvider Authentication { get; set; }
+
+		[Inject]
+		private ISessionStorageService SessionStorage { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			string fullName = await SessionStorage.GetItemAsync<string>("name");
+			if (fullName.Contains(' '))
+				name = new string(fullName.Take(fullName.IndexOf(' ')).ToArray());
+			else
+				name = fullName;
+
+		}
 	}
 }
