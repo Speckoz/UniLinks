a3362a263cc17f190fdb5cd829ed45a5f3a51c0b
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-05-19 03:29:26 -0500

    Criando projeto asp mvc para substuir o blazor server

---
+++ Speckoz.UniLink/UniLink.Client.Web/wwwroot/js/site.js
@@ -0,0 +1,48 @@
+﻿function GoTo(id) {
+    document.getElementById(id).scrollIntoView(true);
+}
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+function ChangeToLight() {
+    ChangeLinkTheme("css/theme.light.css");
+}
+function ChangeToDark() {
+    ChangeLinkTheme("css/theme.dark.css");
+}
+
+function ChangeLinkTheme(cssFile) {
+    document.getElementById("clientThemeLink").setAttribute("href", cssFile);
+}
+
+function SendAlert(msg) {
+    alert(msg);
+}
+
+function ShowModal(modalId) {
+    $(document).ready(() => $('#' + modalId).modal('show'));
+}
+
+function HideModal(modalId) {
+    $(document).ready(() => $('#' + modalId).modal('hide'));
+}
+
+function shoot(time) {
+    const video = document.getElementById('video');
+    //video.currentTime = time;
+    video.currentTime += 0.25;
+
+    const scaleFactor = 0.25;
+
+    const w = video.videoWidth * scaleFactor;
+    const h = video.videoHeight * scaleFactor;
+
+    const canvas = document.getElementById('canvasid');
+    canvas.width = w;
+    canvas.height = h;
+
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, w, h);
+}
