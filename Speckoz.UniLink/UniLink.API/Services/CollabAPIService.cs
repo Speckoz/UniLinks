a47797e8b0d6444b82bdae54c5396e9389c2a781
Marco Antonio P <marcopandolfo577@gmail.com>
2020-05-23 21:44:20 -0300

    Criando service para pegar a aula na api da collab [Api]

---
+++ Speckoz.UniLink/UniLink.API/Services/CollabAPIService.cs
@@ -0,0 +1,33 @@
+﻿using Dependencies.Services;
+using RestSharp;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using UniLink.Dependencies.Data.VO;
+
+namespace UniLink.API.Services
+{
+    public class CollabAPIService
+    {
+        public async Task<CollabVO> GetRecordingInfo(int recordId)
+        {
+            IRestResponse response = await SendRequestTaskAsync(recordId);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+                return JsonSerializer.Deserialize<CollabVO>(response.Content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return default;
+
+            static async Task<IRestResponse> SendRequestTaskAsync(int id)
+            {
+                return await new RequestService()
+                {
+                    Method = Method.GET,
+                    URL = "https://us.bbcollab.com/collab/api/csa/recordings/",
+                    URN = $"{id}/data",
+                }.ExecuteTaskAsync();
+            }
+        }
+    }
+}
