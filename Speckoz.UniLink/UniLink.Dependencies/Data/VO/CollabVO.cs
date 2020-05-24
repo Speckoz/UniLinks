d9ba55ab10b1888b1ebac591d790eb454aff1626
Marco Antonio P <marcopandolfo577@gmail.com>
2020-05-23 21:11:10 -0300

    Criando classe do objecto da api da collab

---
+++ Speckoz.UniLink/UniLink.Dependencies/Data/VO/CollabVO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniLink.Dependencies.Data.VO
+{
+    public class CollabVO
+    {
+        public DateTime Created { get; set; }
+        public DateTime Duration { get; set; }
+        public string Name { get; set; }
+    }
+}
