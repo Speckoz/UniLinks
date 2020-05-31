199ce982391945416086e8d2acb73408afcf3af9
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-05-31 17:02:33 -0500

    Adicionando service para pegar as aulas do aluno [Site]

---
+++ Speckoz.UniLinks/UniLinks.Client.Web/Services/Student/LessonService.cs
@@ -0,0 +1,43 @@
+﻿using Dependencies.Services;
+
+using RestSharp;
+using RestSharp.Authenticators;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using UniLinks.Dependencies.Data.VO.Lesson;
+using UniLinks.Dependencies.Helper;
+
+namespace UniLinks.Client.Site.Services.Student
+{
+	public class LessonService
+	{
+		public async Task<List<LessonDisciplineVO>> GetAllLessonsTaskAync(string token, string disciplines)
+		{
+			var dis = disciplines.Split(';').ToList();
+
+			IRestResponse response = await SendRequestTaskAsync(token, dis);
+
+			if (response.StatusCode == HttpStatusCode.OK)
+				return JsonSerializer.Deserialize<List<LessonDisciplineVO>>(response.Content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+			return null;
+		}
+
+		private async Task<IRestResponse> SendRequestTaskAsync(string token, List<string> disciplines)
+		{
+			return await new RequestService()
+			{
+				Method = Method.POST,
+				URL = DataHelper.URLBase,
+				URN = $"lessons",
+				Body = disciplines,
+				Authenticator = new JwtAuthenticator(token)
+			}.ExecuteTaskAsync();
+		}
+	}
+}
