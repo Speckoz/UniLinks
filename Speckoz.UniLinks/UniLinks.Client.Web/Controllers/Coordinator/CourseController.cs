cbb6a74a78e2752ad66d5e29f388ab2347993a6a
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-02 02:17:10 -0500

    Adicionando pagina para mostrar as informaçoes do curso do coordenador [Site]

---
+++ Speckoz.UniLinks/UniLinks.Client.Web/Controllers/Coordinator/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Threading.Tasks;
+
+using UniLinks.Client.Web.Services.Coordinator;
+using UniLinks.Dependencies.Attributes;
+using UniLinks.Dependencies.Data.VO;
+using UniLinks.Dependencies.Enums;
+
+namespace UniLinks.Client.Web.Controllers.Coordinator
+{
+	[Route("coordinator/course")]
+	public class CourseController : Controller
+	{
+		[HttpGet]
+		[Authorizes(UserTypeEnum.Coordinator)]
+		public async Task<IActionResult> Course([FromServices] CourseService courseService)
+		{
+			string token = User.FindFirst("Token").Value;
+
+			CourseVO course = await courseService.GetCourseByCoordIdTaskAsync(token);
+
+			return View("/Views/Coordinator/Course.cshtml", course);
+		}
+	}
+}
