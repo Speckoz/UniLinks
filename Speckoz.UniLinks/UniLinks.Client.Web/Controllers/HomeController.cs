68eb52c2437ed40ec4641208b79af8fd1963dc51
Marco Antonio P <marcopandolfo577@gmail.com>
2020-06-03 02:50:04 -0300

    Mostrando o hash do ultimo commit

--- Speckoz.UniLinks/UniLinks.Client.Web/Controllers/HomeController.cs
+++ Speckoz.UniLinks/UniLinks.Client.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 
 using System.Threading.Tasks;
+using static System.Environment;
 
 using UniLinks.Dependencies.Attributes;
 
@@ -11,7 +12,11 @@ namespace UniLinks.Client.Web.Controllers
 	public class HomeController : Controller
 	{
 		[HttpGet]
-		public IActionResult Index() => View();
+		public IActionResult Index()
+		{
+			ViewBag.version = GetEnvironmentVariable("LAST_COMMIT")?.Substring(0, 5) ?? "Local";
+			return View();
+		}
 
 		[HttpGet("problem/404")]
 		public IActionResult PageNotFound() => View();
